cfaea8b13d82e377dba2b3dd6ddb7b16a7a4f58d
QimingZheng <zqmmqz@sjtu.edu.cn>
2018-12-11 16:11:53 +0800

    naive parser done

---
+++ parser.cc
@@ -0,0 +1,175 @@
+#include "parser.h"
+
+AST::AST(char *regex, int length){
+    vector<int> entended_regex = extend_regex(regex, length);
+    vector<int> post_exp = re2post(entended_regex);
+    constructAST(post_exp);
+    return;
+}
+
+void AST::constructAST(vector<int> post_regex){
+    stack<ast_node *> S;
+    root = nullptr;
+    for(int i=0;i<post_regex.size();i++){
+        struct ast_node* cur = new ast_node((post_regex[i]>=0), 
+                    (post_regex[i]<0?post_regex[i]:-1), (post_regex[i]>=0?post_regex[i]:-1));
+        if (cur->node_type==0){
+            switch (cur->op_type){
+                case -3:
+                    assert(!S.empty());
+                    cur->child.push_back(S.top());
+                    S.pop();
+                    S.push(cur);
+                    break;
+                case -4:
+                    assert(S.size()>=2)
+                    cur->child.push_back(S.top());
+                    S.pop();
+                    cur->child.push_back(S.top());
+                    S.pop();
+                    S.push_back(cur);
+                    break;
+                case -5:
+                    assert(S.size()>=2)
+                    cur->child.push_back(S.top());
+                    S.pop();
+                    cur->child.push_back(S.top());
+                    S.pop();
+                    S.push_back(cur);
+                    break;
+                default:
+                    printf("ast construction failed\n");
+                    exit(-3);
+                    break;
+            }
+        }
+        else{
+            S.push_back(cur);
+        }
+    }
+    assert(S.size()==1);
+    root = S.top();
+    return;
+}
+
+// -1: concate, -2:alternate, -3: kleene star
+vector<int> AST::re2post(vector<int> re){
+    vector<int> ret;
+    ret.clear();
+
+    stack<int> S;
+
+    for(int i=0 ; i<re.size(); i++){
+        int ch = re[i];
+        switch(ch){
+            // larger number ==> higher priority
+            // (, ): -1, -2
+            // *: -3
+            // |: -4
+            // .: -5
+            // char: >=0
+            case -1: // ( : -1
+                S.push(-1);
+                break;
+            case -2:// ) : -2
+                while(!S.empty()){
+                    if(S.top() == -1 ) break;
+                    ret.push_back(S.top());
+                    S.pop();
+                }
+                if (S.empty()) {printf("invalid regex\n") exit(-1);}
+                S.pop();
+            case -3: // *: -3
+                if(S.empty()) {S.push(-3);}
+                else if (S.top()==-1){
+                    S.push(-3);
+                }
+                else if(S.top()>=-3) {
+                    while(!S.empty()){
+                        if (S.top()<-3 || S.top()==-1) break;
+                        ret.push_back(S.top());
+                        S.pop();
+                    }
+                    S.push(-3);
+                }
+                else{S.push(-3);}
+                break;
+            case -4: // |: -4
+                if(S.empty()) {S.push(-4);}
+                else if (S.top()==-1){
+                    S.push(-4);
+                }
+                else if(S.top()>=-4) {
+                    while(!S.empty()){
+                        if (S.top()<-4 || S.top()==-1) break;
+                        ret.push_back(S.top());
+                        S.pop();
+                    }
+                    S.push(-4);
+                }
+                else{S.push(-4);}
+                break;
+            case -5:
+                if(S.empty()) {S.push(-5);}
+                else if (S.top()==-1){
+                    S.push(-5);
+                }
+                else if(S.top()>=-5) {
+                    while(!S.empty()){
+                        if (S.top()<-5 || S.top()==-1) break;
+                        ret.push_back(S.top());
+                        S.pop();
+                    }
+                    S.push(-5);
+                }
+                else{S.push(-5);}
+                break;
+            default:
+                ret.push_back(ch);
+                break;
+        }
+    }
+
+    while(!S.empty()){
+        if (S.top() == -1) {printf("invalid regex\n"); exit(-2);}
+        ret.push_back(S.top());
+        S.pop();
+    }
+
+    return ret;
+}
+
+vector<int> AST::extend_regex(char *regex, int length){
+    vector<int> re;
+    re.clear();
+
+    for(int i=0; i<length; i++){
+        switch(regex[i]){
+            // (:-1 ):-2  *:-3 |:-4 .:-5
+            case '(':
+                if (re.size()){
+                    int last = re[re.size()-1];
+                    if (last==-2 || last ==-3 || last>=0)
+                        re.push_back(-5);
+                }
+                re.push_back(-1);
+                break;
+            case ')':
+                re.push_back(-2);
+                break;
+            case '*':
+                re.push_back(-3);
+                break;
+            case '|':
+                re.push_back(-4);
+            default:
+                if (re.size()){
+                    int last = re[re.size()-1];
+                    if(last>=0 || last==-2 || last == -3) re.push_back(-5);
+                }
+                re.push_back(int((u8)(regex[i])));
+                break;
+        }
+    }
+    return re;
+}
---
+++ parser.h
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <queue>
+#include <set>
+
+typedef u8 unsigned char;
+
+class AST{
+public:
+    AST(char *regex, int length); // construct ast from original regular expression
+
+    struct ast_node{
+        ast_node(int nt, int ot, int idt) {
+            node_type = nt;
+            op_type = ot;
+            identifier = idt;
+            child.clear();
+        }
+        int node_type; // 0: internal node or 1: leaf, -1: by default at initialization stage
+        int op_type; // if node_type is internal node, then op_type is valid. 0: concate, 1: alternate, 2: kleene star
+                     // we only support 3 types, for the construction of a glushkov NFA.
+        int identifier; // if node_type is leaf, then identifier is valid, represent a character.
+        vector<ast_node *> child;
+    };
+
+    ast_node * root;
+
+    vector<int> re2post(vector<int> re);
+    vector<int> extend_regex(char *regex, int length); // extend user's regex input to standard regex (with .)
+    void constructAST(vector<int> post_regex);
+
+};
