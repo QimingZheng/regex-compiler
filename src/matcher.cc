161dc8e5e2532bc8e425e21136427fe5ea743e26
QimingZheng <zqmmqz@sjtu.edu.cn>
2018-12-13 10:50:08 +0800

    fix naive matcher bug

--- src/matcher.cc
+++ src/matcher.cc
@@ -4,7 +4,7 @@ vector<int> NFA_Matcher::naive_matcher(u8 *str, int length){
     vector<int> ret;
     state_node *cur = nfa->entrance;
     map <int, state_node*> Map[0]; // state_id -> state_node
-    Map[1].insert(pair(cur->state_id, cur));
+    Map[1].insert(make_pair(cur->state_id, cur));
     int ind = 0;
 
     if (cur->is_final) ret.push_back(ind);
@@ -12,12 +12,12 @@ vector<int> NFA_Matcher::naive_matcher(u8 *str, int length){
     while(ind<length){
         for(auto iter = Map[1+ind%2].begin(); iter!=Map[1+ind%2].end(); iter++)
         {
-            for(auto j = (*iter)->out_state.begin() ; j!=(*iter)->out_state.end() ; j++)
-                Map[ind%2].insert(pair((*j)->state_id, (*j)));
+            for(auto j = (iter->second)->out_state.begin() ; j!=(iter->second)->out_state.end() ; j++)
+                Map[ind%2].insert(make_pair((*j)->state_id, (*j)));
         }
         for(auto iter = Map[ind%2].begin(); iter!=Map[ind%2].end(); iter++)
         {
-            if ((*iter)->is_final){
+            if ((iter->second)->is_final){
                 ret.push_back(ind);
                 break;
             }
