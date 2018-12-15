f9b49f1a8667117cd549c7d00b95ce4768ecf902
QimingZheng <zqmmqz@sjtu.edu.cn>
2018-12-15 12:28:05 +0800

    makefile

---
+++ unit/matcher_UT.cc
@@ -0,0 +1,32 @@
+#include "../src/matcher.cc"
+
+int main(){
+    char regex[]="(asd|fns*daj)*|fsj(dav)*f(fs|da)";
+    int length = strlen(regex);
+    
+    AST ast(regex, length);
+    //ast.traverse();
+    
+    GlushKov_NFA NFA(&ast);
+    //NFA.traverse();
+    
+    NFA_Matcher Matcher(&NFA);
+
+    u8 input_str_0[] = "asfnsssdajsjdavdavffda";
+    length = 22;
+
+    vector<int> tmp = Matcher.naive_matcher(input_str_0, length);
+    cout<<tmp.size()<<endl;
+    tmp = Matcher.optimized_matcher(input_str_0, length);
+    cout<<tmp.size()<<endl;
+
+    u8 input_str_1[] = "fsjdavdavdavdavdavdavdavffffda";
+    length = 30;
+    
+    tmp = Matcher.naive_matcher(input_str_1, length);
+    cout<<tmp.size()<<endl;
+    tmp = Matcher.optimized_matcher(input_str_1, length);
+    cout<<tmp.size()<<endl;
+
+    return 0;
+}
